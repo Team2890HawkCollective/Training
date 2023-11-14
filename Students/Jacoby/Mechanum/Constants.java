d7c38a126db85f75cf2b7eea5c9ac5edfa7f5dfb
Jacoby-Powell <Jacobyp7427@gmail.com>
2023-11-14 10:13:51 -0500

    Jacoby Powell's Attempt at Training 49, Mechanum Implementation. Let me know if this qualifies for the ticket.

---
+++ Students/Jacoby/Mechanum/Constants.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package edu.wpi.first.wpilibj.templates.commandbased;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants 
+{
+    public static final int MOTOR_FRONT_LEFT = 1;
+    public static final int MOTOR_FRONT_RIGHT = 2;
+    public static final int MOTOR_BACK_LEFT = 3;
+    public static final int MOTOR_BACK_RIGHT = 4;
+
+    public static final int DRIVER_XBOX_CONTROLLER_PORT = 0;
+
+
+
+
+    public static final double SPEED_MOD = 1;
+
+    public static final int POLARITY_SWAP = -1;
+
+    public static final double frontLeftMotorCoeff = 1.0;
+    public static final double frontRightMotorCoeff = 1.0;
+    public static final double backLeftMotorCoeff = 1.0;
+    public static final double backRightMotorCoeff = 1.0;
+    
+    public static final int FRONT_LEFT_POLARITY = -1;
+    public static final int FRONT_RIGHT_POLARITY = 1;
+    public static final int BACK_LEFT_POLARITY = 1;
+    public static final int BACK_RIGHT_POLARITY = -1;
+}
---
+++ Students/Jacoby/Mechanum/commands/DriveTrainCommand.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+/** An example command that uses an example subsystem. */
+public class DriveTrainCommand extends CommandBase 
+{
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DriveTrain m_subsystem;
+
+
+
+  public DriveTrainCommand(DriveTrain subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+
+  @Override
+  public void execute() 
+  {
+
+  }
+
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
