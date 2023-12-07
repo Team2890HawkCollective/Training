6724cfe37606488d91d4c53087a4567d42a110d4
FRC <frc@hhs-153-18.cps.local>
2023-12-07 16:30:28 -0500

    Updated training template to (hopefully) meet ticket requirements

--- Assignments/RobotCodeTemplates/CommandBase/Constants.java
+++ Assignments/RobotCodeTemplates/CommandBase/Constants.java
@@ -12,4 +12,27 @@ package edu.wpi.first.wpilibj.templates.commandbased;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+  
+    //Wheels
+    public static final int MOTOR_FRONT_LEFT = 1;
+    public static final int MOTOR_FRONT_RIGHT = 2;
+    public static final int MOTOR_BACK_LEFT = 3;
+    public static final int MOTOR_BACK_RIGHT = 4;
+
+    //Controllers
+    public static final int DRIVER_XBOX_CONTROLLER_PORT = 0;
+
+    //Wheel motor coefficients
+    public static final double frontLeftMotorCoeff = 1.0;
+    public static final double frontRightMotorCoeff = 1.0;
+    public static final double backLeftMotorCoeff = 1.0;
+    public static final double backRightMotorCoeff = 1.0;
+
+    //Polarity modifiers
+    public static final int FRONT_LEFT_POLARITY = -1;
+    public static final int FRONT_RIGHT_POLARITY = 1;
+    public static final int BACK_LEFT_POLARITY = 1;
+    public static final int BACK_RIGHT_POLARITY = -1;
+
+}
